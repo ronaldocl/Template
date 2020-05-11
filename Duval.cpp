7be67a121c65cf34445706c8e7df0c517df5958c
ronaldocl <emcaolei@163.com>
2020-05-12 00:12:46 +0800

    add Duval.cpp

--- Duval.cpp
+++ Duval.cpp
@@ -48,3 +48,55 @@ void debug_out() { cerr << endl; }
 template <typename Head, typename... Tail>
 void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); }
 #define dbg(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+
+
+vector<string> duval(string const& s) {
+    int n = s.size();
+    int i = 0;
+    vector<string> factorization;
+    while (i < n) {
+        int j = i + 1, k = i;
+        while (j < n && s[k] <= s[j]) {
+            if (s[k] < s[j])
+                k = i;
+            else
+                k++;
+            j++;
+        }
+        while (i <= k) {
+            factorization.push_back(s.substr(i, j - k));
+            i += j - k;
+        }
+    }
+    return factorization;
+}
+
+
+string min_cyclic_string(string s) {
+    s += s;
+    int n = s.size();
+    int i = 0, ans = 0;
+    while (i < n / 2) {
+        ans = i;
+        int j = i + 1, k = i;
+        while (j < n && s[k] <= s[j]) {
+            if (s[k] < s[j])
+                k = i;
+            else
+                k++;
+            j++;
+        }
+        while (i <= k)
+            i += j - k;
+    }
+    return s.substr(ans, n / 2);
+}
+
+
+int main(){ 
+    string s = "bcacbaba";
+    dbg(duval(s));
+    cout << min_cyclic_string(s) << endl;
+
+    return 0;
+}
